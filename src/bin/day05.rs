a78cb4e975d470d49e961b5ea962d8560a383b8b
toteload <toteload@users.noreply.github.com>
2023-12-05 22:39:15 +0100

    Day 5 part 2 wip
    I misread the problem, wasted a lot of time and now I'm tired.

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -1,9 +1,13 @@
+use core::ops::Range;
+
 const INPUT: &str = include_str!("../../input/day_05.txt");
 
 fn parse_nums(section: &str) -> Vec<u64> {
     let start = section.bytes().position(|b| b == b':').unwrap();
     let list = &section[start + 1..].trim();
-    list.split_ascii_whitespace().map(|s| s.parse::<u64>().unwrap()).collect()
+    list.split_ascii_whitespace()
+        .map(|s| s.parse::<u64>().unwrap())
+        .collect()
 }
 
 #[derive(Debug)]
@@ -11,6 +15,16 @@ struct Entry {
     start_src: u64,
     start_dst: u64,
     len: u64,
+}
+
+impl Entry {
+    fn src_range(&self) -> Range<u64> {
+        self.start_src..self.start_src + self.len
+    }
+
+    fn src_end(&self) -> u64 {
+        self.start_src + self.len
+    }
 }
 
 fn parse_map(section: &str) -> Vec<Entry> {
@@ -73,8 +87,117 @@ fn part_1(input: &str) {
     println!("{answer}");
 }
 
+fn map_range(map: &[Entry], range: &Range<u64>) -> Vec<Range<u64>> {
+    let lo_idx = map.partition_point(|&Entry { start_src, .. }| start_src <= range.start);
+    let hi_idx = map.partition_point(|&Entry { start_src, .. }| start_src <= range.end);
+
+    //println!("{map:?}");
+    //println!("{lo_idx}, {hi_idx}");
+
+    let mut range = range.start..range.end;
+    let mut res = Vec::new();
+
+    assert!(lo_idx != 0);
+    assert!(hi_idx != 0);
+
+    if lo_idx == map.len() {
+        return vec![range];
+    }
+
+    if hi_idx == map.len() {
+        let e = map.last().unwrap();
+        let start = e.start_src + e.len;
+        return vec![start..range.end];
+    }
+                //println!("{map:?}, {range:?}, {lo_idx}, {hi_idx}");
+
+    for at in (lo_idx-1)..hi_idx {
+        if range.is_empty() {
+            break;
+        }
+
+        if !(map[at].src_range().contains(&range.start)) {
+            println!("{map:?}, {range:?}, {lo_idx}, {hi_idx}");
+        }
+
+        assert!(map[at].src_range().contains(&range.start));
+
+        //if !map[at].src_range().contains(&range.start) {
+        //    if !(map[at].start_src > range.start) {
+        //        println!("{map:?}, {range:?}, {lo_idx}, {hi_idx}");
+        //    }
+
+        //    res.push(range.start..map[at].start_src);
+        //    range = map[at].start_src..range.end;
+        //}
+
+        let end = range.end.min(map[at].src_end());
+        res.push(
+            (range.start + map[at].start_dst - map[at].start_src)
+                ..(end + map[at].start_dst - map[at].start_src),
+        );
+        range = end..range.end;
+    }
+
+    res
+}
+
+fn fill_gaps(map: Vec<Entry>) -> Vec<Entry> {
+    let n = map.len();
+    let mut res = Vec::new();
+
+    for i in 1..n {
+        if map[i-1].start_src+map[i-1].len < map[i].start_src {
+            res.push(Entry {
+                start_src: map[i-1].start_src+map[i-1].len,
+                start_dst: map[i-1].start_src+map[i-1].len,
+                len: map[i].start_src - map[i-1].start_src+map[i-1].len,
+            });
+        }
+    }
+
+    res.extend(map);
+    res.sort_by_key(|e| e.start_src);
+
+    res
+}
+
 fn part_2(input: &str) {
-    todo!()
+    let sections = input.split("\n\n").collect::<Vec<_>>();
+    let seeds = parse_nums(sections[0]);
+
+    let seed_ranges = seeds
+        .chunks_exact(2)
+        .map(|nums| {
+            let &[start, len] = nums else { unreachable!() };
+            start..(start+len)
+        })
+        .collect::<Vec<_>>();
+
+    let maps = sections[1..]
+        .iter()
+        .map(|&s| fill_gaps(parse_map(s)))
+        .collect::<Vec<_>>();
+
+    //println!("{:?}", map_range(&maps[1], &(14..50)));
+
+    println!("{seed_ranges:?}");
+
+    let mut last_ranges = seed_ranges;
+    for map in maps {
+        let mut out = Vec::new();
+        for r in last_ranges {
+            out.extend(map_range(&map, &r));
+        }
+        println!("{out:?}");
+        last_ranges = out;
+    }
+
+    //println!("{last_ranges:?}");
+
+    //let answer = last_ranges.iter().map(|r| r.start).min().unwrap();
+
+    //println!("{answer}");
 }
 
 fn main() {
